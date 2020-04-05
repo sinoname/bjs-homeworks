cb66c34657f4cf6705de7a93747f0127fee7a126
Aziz Rozyev <pirrat@live.ru>
2020-04-05 20:51:58 +0300

    Code added

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,38 @@
+'use strict';
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    if (isNaN(percent)) {
+        console.log(`Параметр percent содержит неправильное значение ${percent}`);
+    } else {
+        percent = parseInt(percent);   
+    }
+    if (isNaN(contribution)) {
+        console.log(`Параметр contribution содержит неправильное значение ${contribution}`);
+    } else {
+        contribution = parseInt(contribution);
+    }  
+    if (isNaN(amount)) {
+        console.log(`Параметр amount содержит неправильное значение ${amount}`);
+    } else {
+        amount = parseInt(amount);
+    }   
+    let S = amount - contribution;
+    let n = Number((date.getFullYear() - new Date().getFullYear()) * 12 + (date.getMonth()) - (new Date().getMonth()));    
+    let P = percent / 12 / 100; 
+    let payment =  S * (P + P / (Math.pow((1 + P), n)- 1 ));
+    let totalAmount = payment * n;
+    console.log(+totalAmount.toFixed(2));
+    return +totalAmount.toFixed(2);
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    let greeting = 'Привет, мир! Меня зовут';
+    if (name === undefined || name === null || name === '') {
+        console.log(`${greeting} Аноним`);
+        return `${greeting} Аноним`;
+    } else {
+        console.log(greeting + name);
+    }
+
+    return `${greeting} ${name}`;
 }
