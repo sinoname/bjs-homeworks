1678e1c3eeecc9b6cd664a6e47abd5241110a7a1
Aziz Rozyev <pirrat@live.ru>
2020-04-14 19:24:01 +0300

    First commit object-prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,39 @@
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+    if (animal === undefined) {
+        return null;
+    }
+
+    let sound = animal.sound;
+    return sound;
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let average = 0;
+	let roundedAverage = 0;
+	if (marks.length == 0) {
+		return 0;
+	}
+	for (let i = 0; i < marks.length; i++) {
+	   average += Number(marks[i]);	   
+	   roundedAverage = average / marks.length;
+	}
+	
+	return Math.round(roundedAverage);
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = Date.now();
+    let InfoDate = Date.parse(birthday);
+    let diff = now - InfoDate;
+    let milliSecondsYear = 1000 * 60 * 60 * 24 * 365.25;
+    let age = diff / milliSecondsYear;
+    let ageYears = Math.floor(age);
+    let verdict;
+    if (ageYears >= 18) {
+        verdict = true;
+    } else {
+        verdict = false;
+    }
+   
+    return verdict;
 }
